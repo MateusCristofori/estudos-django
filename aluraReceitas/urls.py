bea064c0a4a3afe759df100833d2cbc766de5f11
mateus cristofori <mateuscristofori@gmail.com>
2022-07-14 10:01:45 -0300

    aula 01(alura) curso Django

--- aluraReceitas/urls.py
+++ aluraReceitas/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', include('receitas.urls')),
+    path('home/', include('receitas.urls')),
     path('admin/', admin.site.urls)
 ]
