8efcb9b5c3742accb4cc0d0286aeb6747a2d4e59
mateus cristofori <mateuscristofori@gmail.com>
2022-07-14 17:58:35 -0300

    Buscando receitas

--- receitas/views.py
+++ receitas/views.py
@@ -20,3 +20,17 @@ def receita(request, receita_id):
   }
   
   return render(request, 'receita.html', receita_a_exibir)
+
+
+def buscar(request):
+  buscar_receita = Receita.objects.order_by('-data_receita').filter(publicada=True)
+  
+  if 'buscar' in request.GET:
+    nome_a_buscar = request.GET['buscar']
+    if 'buscar':
+      buscar_receita = buscar_receita.filter(nome_receita__icontains=nome_a_buscar)
+  
+  dados = {
+    'receitas': buscar_receita
+  }
+  return render(request, 'buscar.html', dados)
