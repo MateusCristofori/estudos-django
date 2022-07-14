b49fbc99d73da29003feafe702555b82d02705f8
mateus cristofori <mateuscristofori@gmail.com>
2022-07-14 12:08:08 -0300

    relacionando classes com foreign key e alterando o nome das pessoas.

--- pessoas/models.py
+++ pessoas/models.py
@@ -1,7 +1,8 @@
+from tabnanny import verbose
 from django.db import models
 
 class Pessoas(models.Model):
   nome = models.CharField(verbose_name='Digite seu nome', max_length=200)
-  email = models.EmailField(max_length=200)
+  email = models.EmailField(verbose_name='Digite seu email', max_length=200)
   def __str__(self) -> str:
     return self.nome
