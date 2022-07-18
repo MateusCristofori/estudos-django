4ec134edb43a4a3001a9706e3bcee211d6fb8994
mateus cristofori <mateuscristofori@gmail.com>
2022-07-18 17:14:25 -0300

    criação dos campos de cadastro e login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,17 @@
-from unicodedata import name
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def cadastro(request):
-  return render(request, 'usuarios/cadastro.html')
+  
+  if request.method == 'POST':
+    nome = request.POST['nome']
+    email = request.POST['email']
+    senha = request.POST['password']
+    confirmacao_senha = request.POST['password2']    
+    print(nome, email, senha, confirmacao_senha)
+    return redirect('login')
+  else:
+    return render(request, 'usuarios/cadastro.html')
 
 def login(request):
   return render(request, 'usuarios/login.html')
