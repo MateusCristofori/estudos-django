8946ca5606088b3d818192fbe24427cdbfe4bb8a
mateus cristofori <mateuscristofori@gmail.com>
2022-07-20 17:10:06 -0300

    Criando as receitas e salvando no banco de dados

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth
-
 from receitas.models import Receita
 
 
@@ -54,8 +53,16 @@ def login(request):
 
 
 def dashboard(request):
+
     if request.user.is_authenticated:
-        return render(request, 'usuarios/dashboard.html')
+        id = request.user.id
+        receitas = Receita.objects.order_by('-data_receita').filter(pessoa=id)
+
+        dados = {
+            'receitas': receitas
+        }
+
+        return render(request, 'usuarios/dashboard.html', dados)
     else:
         return redirect('index')
 
@@ -74,8 +81,10 @@ def cria_receita(request):
         tempo_preparo = request.POST['tempo_preparo']
         rendimento = request.POST['rendimento']
         categoria = request.POST['categoria']
-        print(nome_receita, ingredientes, modo_preparo,
-              tempo_preparo, rendimento, categoria)
+        user = get_object_or_404(User, pk=request.user.id)
+        receita = Receita.objects.create(pessoa=user, nome_receita=nome_receita, ingredientes=ingredientes,
+                                         modo_preparo=modo_preparo, tempo_preparo=tempo_preparo, rendimento=rendimento, categoria=categoria)
+        receita.save()
         return redirect('dashboard')
     else:
         return render(request, 'usuarios/cria_receita.html')
